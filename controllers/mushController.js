ef7f833a0c1ba61536f01372b699de31f21b15e3
D.K Nam <Dataplant@outlook.com>
2022-05-15 18:31:56 -0400

    updated controller js

--- controllers/mushController.js
+++ controllers/mushController.js
@@ -2,5 +2,11 @@ const router = require('express').Router();
 const db = require('../models/index.js')
 
 router.get('/', (req, res) => {
-    db.Mushrooms
+    db.Mushrooms.find({})
+    .populate('EstiTime')
+    .exec((err, foundShrooms) => {
+        if (err) return console.log(err);
+        res.json(foundShrooms)
+        console.log('populate check complete')
+    })
 })
